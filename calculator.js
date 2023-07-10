9e7f532b867d0462c3e2f7c8547e5fc7c116a154
Tobias Bertera <tobiasbertera@gmail.com>
2023-07-10 12:05:20 -0300

    Add initialized variables and operate function

--- calculator.js
+++ calculator.js
@@ -15,3 +15,27 @@ function multiply (a, b) {
 function division (a, b) {
     return a / b;
 }
+
+// variables to keep the 3 parts of the calculation operation //
+
+let numberA;
+
+let operator;
+
+let numbreB;
+
+// function OPERATE //
+
+function operate(numberA, operator, numbreB) {
+    
+    if (operator === "+") {
+        sum(numberA, numbreB)
+    } else if (operator === "-") {
+        subtract(numberA, numbreB)
+    } else if (operator === "x"){
+        multiply(numberA, numbreB) 
+    } else if (operator === "%") {
+        division(numberA, numbreB)
+    }
+
+}
