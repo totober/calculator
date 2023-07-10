aed13e08ab5010acbb4cbdaa76bc74e4221721d3
Tobias Bertera <tobiasbertera@gmail.com>
2023-07-10 11:27:30 -0300

    Add 4 basic functions of the calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+// functions for the calculator //
+
+function sum (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function division (a, b) {
+    return a / b;
+}
